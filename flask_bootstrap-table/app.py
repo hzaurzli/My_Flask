dc82549cc7bd2496e54fc47555491b4e37d22495
Small runze <yun_act@163.com>
2020-10-07 10:20:58 +0800

    Update app.py

--- flask_bootstrap-table/app.py
+++ flask_bootstrap-table/app.py
@@ -29,3 +29,41 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# from flask import Flask, render_template
+# from flask_paginate import Pagination, get_page_args
+# 
+# 
+# app = Flask(__name__)
+# # app.template_folder = ''
+# users = list('w'*200)
+# 
+# # users = ['w','e','a','daw','dw','dwadw','dwaq','few']
+# # print(users)
+# 
+# def get_users(offset, per_page):
+#     return users[offset: offset + per_page]
+# 
+# 
+# @app.route('/')
+# def index():
+#     page, per_page, offset = get_page_args(page_parameter='page',
+#                                            per_page_parameter='per_page')
+#     total = len(users)
+#     pagination_users = get_users(offset=0, per_page=30)
+#     pagination = Pagination(page=page, per_page=per_page, total=total,
+#                             css_framework='bootstrap4')
+#     return render_template('index.html',
+#                            users=pagination_users,
+#                            page=page,
+#                            per_page=per_page,
+#                            pagination=pagination,
+#                            )
+# 
+# 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
