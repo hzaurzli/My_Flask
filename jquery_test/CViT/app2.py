1c3e019c7ec385d90037cd8d0ed2c35c988b3ac8
Small runze <yun_act@163.com>
2023-01-01 14:15:53 +0800

    Add files via upload

---
+++ jquery_test/CViT/app2.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/up_vue', methods=['GET', 'POST'])  # 接受并存储文件
+def up_file():
+    if request.method == "POST":
+        # 接收图片
+        f = request.files['file']
+        print(f.filename)
+        f.save(f.filename)
+        return 'Upload successful'
+
+
+# @app.route('/', methods=['get', 'post'])
+# def index():
+#     return render_template('vueupload.html')
+
+@app.route('/', methods=['get', 'post'])
+def index():
+    return render_template('cvit.html')
+
+if __name__ == "__main__":
+    app.run()
