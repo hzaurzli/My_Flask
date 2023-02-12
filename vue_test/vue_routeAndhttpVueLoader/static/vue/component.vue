9035f8fd978f71534f77f8305e4432ac5790f323
Small runze <yun_act@163.com>
2023-02-12 22:46:14 +0800

    Add files via upload

---
+++ vue_test/vue_routeAndhttpVueLoader/static/vue/component.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='kaimo-info'>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+module.exports = {
+    name: 'KaimoInfo',
+    data () {
+        return {
+            msg: "hello http-vue-loader --- kaimo "
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
